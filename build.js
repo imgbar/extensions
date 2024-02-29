16f95246804a784b30e75f2ded7558f354d2303c
Alabhya Jindal <alabhya10@gmail.com>
2024-02-29 17:05:38 +0530

    Init

---
+++ build.js
@@ -0,0 +1,37 @@
+const AdmZip = require('adm-zip')
+const args = process.argv.slice(2)
+
+if (args.length === 0) {
+  console.log('Usage: node build.js <extension to build>')
+  process.exit(1)
+}
+
+const extension = args[0]
+if (extension !== 'firefox' && extension !== 'chrome') {
+  console.log('Only firefox and chrome extension building is supported')
+  process.exit(1)
+}
+
+const isWatching = args[1] === 'watch'
+
+// Load the manifest json to grab version information and name.
+const manifest = require(`./${extension}/manifest.json`)
+const version = manifest.version
+
+/*
+ * We want to zip up the relevant extension files, which would be
+ * all `shared/*` files and then the relevant `extension/*` files.
+ * This approach seemed easier than copying some things around to a temp directory.
+ * And! It's cross-platform!
+ */
+const zip = new AdmZip()
+zip.addLocalFolder('shared')
+zip.addLocalFolder(extension)
+
+zip.writeZip(`${__dirname}/built/imgbar_${extension}_${version}.zip`)
+console.log(`Done: built/imgbar_${extension}_${version}.zip`)
+
+if (isWatching) {
+  zip.extractAllTo(`${__dirname}/built/`, true)
+  console.log(`Done: Extracted built/imgbar_${extension}_${version}.zip`)
+}
---
+++ shared/src/background.js
@@ -0,0 +1,43 @@
+// Chrome does not support the browser namespace.
+if (typeof browser == 'undefined') {
+  globalThis.browser = chrome;
+}
+
+// Setting BASE_URL which is used to determine the image editor's Url.
+const BASE_URL = 'http://127.0.0.1:5500';
+// const BASE_URL = 'https://imgbar.lol';
+
+/**
+ * Called when the item has been created, or when creation failed due to an error. We'll just log success/failure here.
+ */
+function onCreated() {
+  if (browser.runtime.lastError) {
+    console.log(`Error: ${browser.runtime.lastError}`);
+  } else {
+    console.log('Item created successfully');
+  }
+}
+
+/**
+ * Called when the 'Edit' item is clicked on an image. Opens the Imgbar image editor with the image url in query params.
+ * @param {string} imageUrl
+ */
+async function openImageEditor(imageUrl) {
+  const newUrl = `${BASE_URL}/?image=${encodeURIComponent(imageUrl)}`;
+  browser.tabs.create({ url: newUrl });
+}
+
+// Create the context menu item.
+browser.contextMenus.create({
+  id: 'imgbar-edit',
+  title: 'Edit with Imgbar',
+  contexts: ['image'],
+});
+
+// Register the click event listener and perform the appropriate action given the ID of the context menu.
+browser.contextMenus.onClicked.addListener((info, tab) => {
+  switch (info.menuItemId) {
+    case 'imgbar-edit':
+      openImageEditor(info.srcUrl);
+  }
+});
